c4e41563d105d58f6d33a218a1224388bd2b46fd
kukidevs <kukidevs@gmail.com>
2023-01-23 18:09:08 +0400

    HW 9S 1.1

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,15 +1,13 @@
 package com.example.playlistmaker
 
-import android.content.Intent
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 
 class SearchActivity : AppCompatActivity() {
     var inputTextIn = ""
@@ -22,41 +20,27 @@ class SearchActivity : AppCompatActivity() {
         val backIcon = findViewById<ImageView>(R.id.iconBack)
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        if (savedInstanceState != null) inputTextIn = savedInstanceState.getString(QUERY_LINE).toString()
+        inputEditText.setText(inputTextIn)
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            val view = this.currentFocus
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            if (view!=null) imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
         backIcon.setOnClickListener{
-            val displayIntent = Intent(this, MainActivity::class.java)
-            startActivity(displayIntent)
+            finish()
         }
-        val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
-            }
-
-            @RequiresApi(Build.VERSION_CODES.M)
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = clearButtonVisibility(s)
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // empty
-            }
+        inputEditText.doOnTextChanged { text, _, _, _ ->
+            clearButton.visibility = clearButtonVisibility(text)
+            inputTextIn = text.toString()
         }
-        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        println("saved")
         outState.putString(QUERY_LINE, inputTextIn)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        println("restored")
-        super.onRestoreInstanceState(savedInstanceState)
-        inputTextIn = savedInstanceState.getString(QUERY_LINE, "")
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {

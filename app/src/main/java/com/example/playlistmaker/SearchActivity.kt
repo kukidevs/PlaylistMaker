e045d2e621712e288901a433d1ba3b8dab6b530f
kukidevs <kukidevs@gmail.com>
2023-02-01 03:06:24 +0400

    HW 10S 1.0

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,13 +1,16 @@
 package com.example.playlistmaker
 
-import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doOnTextChanged
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
     var inputTextIn = ""
@@ -20,27 +23,49 @@ class SearchActivity : AppCompatActivity() {
         val backIcon = findViewById<ImageView>(R.id.iconBack)
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
-        if (savedInstanceState != null) inputTextIn = savedInstanceState.getString(QUERY_LINE).toString()
-        inputEditText.setText(inputTextIn)
-
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val tracksArray: ArrayList<Track> = arrayListOf(
+                                                             Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
+                                                             Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
+                                                             Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
+                                                             Track("Whole Gotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
+                                                             Track("Sweet Child O'Mine", "Guns N' Roses", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
+        val tracksAdapter = TracksAdapter(tracksArray)
+        recyclerView.adapter = tracksAdapter
         clearButton.setOnClickListener {
             inputEditText.setText("")
-            val view = this.currentFocus
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            if (view!=null) imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
         backIcon.setOnClickListener{
-            finish()
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
         }
-        inputEditText.doOnTextChanged { text, _, _, _ ->
-            clearButton.visibility = clearButtonVisibility(text)
-            inputTextIn = text.toString()
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            @RequiresApi(Build.VERSION_CODES.M)
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
         }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        println("saved")
         outState.putString(QUERY_LINE, inputTextIn)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        println("restored")
+        super.onRestoreInstanceState(savedInstanceState)
+        inputTextIn = savedInstanceState.getString(QUERY_LINE, "")
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
